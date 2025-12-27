8bc315b18a85e7c2caa5afa35a752e06388679bc
Kohsuke Kawaguchi <kk@kohsuke.org>
2025-12-27 06:40:06 -0800

    Button control and a fragment

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from 'react';
 import './Board.css';
 import Game from './Game.tsx';
+import ActionButton from './components/button.tsx';
 
 export default function Board({ game }: { game: Game }) {
   // 1. A bit of "dummy" state just to force React to re-render
@@ -19,34 +20,37 @@ export default function Board({ game }: { game: Game }) {
   };
 
   return (
-    <div className="puzzle-board" style={gridStyle}>
-      {(()=> {
-        const elements = [];
+    <>
+      <div className="puzzle-board" style={gridStyle}>
+        {(()=> {
+          const elements = [];
 
-        for (let r = 0; r < game.Y*2-1; r++) {
-          for (let c = 0; c < game.X*2-1; c++) {
+          for (let r = 0; r < game.Y*2-1; r++) {
+            for (let c = 0; c < game.X*2-1; c++) {
 
-            const isRowEven = r % 2 === 0;
-            const isColEven = c % 2 === 0;
+              const isRowEven = r % 2 === 0;
+              const isColEven = c % 2 === 0;
 
-            if (isRowEven && isColEven) {
-              const cell = game.cells[c/2][r/2];
-              // not sure why, but this only works if data-version={tick} is there. that must be somehow
-              // forcing a redraw
-              elements.push(<div key={`${r}-${c}`} data-version={tick} className="cell" onClick={()=>cell.click()}>
-                {cell.render()}
-              </div>);
-            } else if (isRowEven && !isColEven) {
-              elements.push(<div key={`${r}-${c}`} className="border-v dotted"></div>);
-            } else if (!isRowEven && isColEven) {
-              elements.push(<div key={`${r}-${c}`} className="border-h solid"></div>);
-            } else {
-              elements.push(<div key={`${r}-${c}`} className="corner"></div>);
+              if (isRowEven && isColEven) {
+                const cell = game.cells[c/2][r/2];
+                // not sure why, but this only works if data-version={tick} is there. that must be somehow
+                // forcing a redraw
+                elements.push(<div key={`${r}-${c}`} data-version={tick} className="cell" onClick={()=>cell.click()}>
+                  {cell.render()}
+                </div>);
+              } else if (isRowEven && !isColEven) {
+                elements.push(<div key={`${r}-${c}`} className="border-v dotted"></div>);
+              } else if (!isRowEven && isColEven) {
+                elements.push(<div key={`${r}-${c}`} className="border-h solid"></div>);
+              } else {
+                elements.push(<div key={`${r}-${c}`} className="corner"></div>);
+              }
             }
           }
-        }
-        return elements;
-        })()}
-    </div>
+          return elements;
+          })()}
+      </div>
+      <ActionButton label="Solve" onAction={() => {game.cells[0][0].click()}} />
+    </>
   );
 };
---
+++ src/components/button.tsx
@@ -0,0 +1,19 @@
+export default function ActionButton({
+  label,
+  onAction
+}: {
+  label: string,
+  onAction: () => void
+}) {
+  return (
+    <button
+      className="game-button"
+      onClick={() => {
+        console.log(`Running computation: ${label}`);
+        onAction();
+      }}
+    >
+      {label}
+    </button>
+  );
+}
