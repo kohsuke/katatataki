ed9ab4599bb3221eeb1c6b1fdb5b9233d17c6044
Kohsuke Kawaguchi <kk@kohsuke.org>
2025-12-27 14:09:15 -0800

    Added the next puzzle

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,43 @@ import './index.css'
 import Board from './components/Board.tsx'
 import Game from './game/Game.tsx'
 
+function translate(s: string) {
+  const mapping = {'T':'た', '=':'＝', 'K':'き', 'A':'か', ' ':'' }
+  s = s.trim();
+  for (const [k,v] of Object.entries(mapping)) {
+    s = s.replaceAll(k, v);
+  }
+
+  return s.split('\n');
+}
+
 const 初級1 = [
-"きたきたた＝",
-"た＝きかたき",
-"かたききたた",
-"たたかたたた",
-"き＝たかかた",
-"かきたたき＝"
+  "きたきたた＝",
+  "た＝きかたき",
+  "かたききたた",
+  "たたかたたた",
+  "き＝たかかた",
+  "かきたたき＝"
 ];
+
+const 中級1 = translate(`
+TT=TTT TKTKTT
+=TKKT= TKTA=T
+KATT=T TTT=TA
+KKTKT= TAKKTK
+TATKTA KTAATK
+TTATAA KKTTTK
+TKTTAA TTTKAT
+=TTKTTT =KT=T
+TTKTTK =T=TTT
+ATKT=K TTT=K=
+K=TTT= T=TTTT
+TTT=KT TKTK=T
+`);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Board game={new Game(初級1)} />
+    <Board game={new Game(中級1)} />
   </StrictMode>,
 )
