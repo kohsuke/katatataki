62eeb05cbe729a3e89ff014afe245968e6d40ba8
Kohsuke Kawaguchi <kk@kohsuke.org>
2025-12-27 09:16:19 -0800

    Direction enum

---
+++ src/game/Direction.tsx
@@ -0,0 +1,18 @@
+export default class Direction {
+  public static readonly U = new Direction(0, 0,-1);
+  public static readonly D = new Direction(1, 0,+1);
+  public static readonly L = new Direction(2, -1,0);
+  public static readonly R = new Direction(3, +1,0);
+
+  public static readonly ALL = [Direction.U, Direction.D, Direction.L, Direction.R];
+
+  public readonly ordinal: number;
+  public readonly dx: number;
+  public readonly dy: number;
+
+  private constructor(ordinal: number, dx: number, dy: number) {
+    this.ordinal = ordinal;
+    this.dx = dx;
+    this.dy = dy;
+  }
+}
